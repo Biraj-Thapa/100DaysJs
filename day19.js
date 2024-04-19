15bd45d76b031f8ad516bc68fa45e6076e3cb83f
Biraj Thapa <birajthapa983@gmail.com>
2024-04-19 13:09:30 +0545

    day 19 added

---
+++ day19.js
@@ -0,0 +1,15 @@
+//write a function to check if a given string starts with a specific substring.
+
+const checkStr = (str, subStr) => {
+  newStr = str.trim();
+  toArr = newStr.split(" ");
+
+  if (toArr[0].toLowerCase() === subStr.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(checkStr(("hello Hii"),("hello")));
+console.log(checkStr(("biraj thapa"),("Biraj")));
+console.log(checkStr(("hii i am biraj"),("hello")))
