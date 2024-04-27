a487c4ddf79444bfbbfb9b11759c595c343bc16b
Biraj Thapa <birajthapa983@gmail.com>
2024-04-27 18:42:14 +0545

    dat 27 added

---
+++ day27.js
@@ -0,0 +1,12 @@
+//Repeat a String
+//write a function called repeatString  that takes two parameters 
+//A string tthat needs to be reppeated.
+//An integer representing number of time the string should be repeated.
+//The function should retrun the input string str the specified number of times and retrun the resulting string.
+
+const repeatedString=(str,num)=>{
+    return str.repeat(num)
+
+
+}
+console.log(repeatedString("biraj",5))
